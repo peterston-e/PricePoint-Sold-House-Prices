16f15a288deea91a2e70344615ce218938b8777b
peterson-e <55142305+peterston-e@users.noreply.github.com>
2024-08-08 11:35:21 +0100

    working mvp1

--- src/app/helpers/houseHelper.js
+++ src/app/helpers/houseHelper.js
@@ -7,9 +7,18 @@ export const houseType = {
 	semi: HouseIcon,
 	terraced: HouseIcon,
 	other: HouseIcon,
+	f: Apartment, // Assuming 'F' stands for Flat
+	s: HouseIcon, // Assuming 'S' stands for Semi-detached
+	t: HouseIcon, // Assuming 'T' stands for Terraced
+	d: HouseIcon, // Assuming 'D' stands for Detached
+	o: OtherIcon, // For 'Other' type
 };
 
 export const getIcon = (type) => {
+	if (!type) {
+		console.warn("getIcon called with undefined type");
+		return null;
+	}
 	const Icon = houseType[type.toLowerCase()];
 	return <Icon className="w-4 h-4 mr-1" />;
 };
@@ -19,11 +28,19 @@ export const houseImage = {
 	detached: "/assets/images/detached.jpeg",
 	semi: "/assets/images/semi.png",
 	terraced: "/assets/images/terrace.jpeg",
-	other: "/assets/images/unknown.jpeg",
+	f: "/assets/images/apartment.jpeg",
+	s: "/assets/images/semi.png",
+	t: "/assets/images/terrace.jpeg",
+	d: "/assets/images/detached.jpeg",
+	o: "/assets/images/unknown.jpeg",
 };
 
 export const getHouseImage = (type) => {
-	return houseImage[type.toLowerCase()];
+	if (!type) {
+		console.warn("getHouseImage called with undefined type");
+		return "/assets/images/unknown.jpeg";
+	}
+	return houseImage[type.toLowerCase()] || "/assets/images/unknown.jpeg";
 };
 
 export const formatPrice = (price) => {
@@ -33,4 +50,23 @@ export const formatPrice = (price) => {
 		maximumFractionDigits: 0,
 		minimumFractionDigits: 0,
 	}).format(price);
+};
+
+export const expandType = (type) => {
+	const typeMap = {
+		d: "Detached",
+		s: "Semi",
+		t: "Terraced",
+		f: "Apartment",
+		o: "Other",
+	};
+	return typeMap[type.toLowerCase()] || type;
+};
+
+export const expandTenure = (tenure) => {
+	const tenureMap = {
+		f: "Freehold",
+		l: "Leasehold",
+	};
+	return tenureMap[tenure.toLowerCase()] || tenure;
 };
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,25 +9,12 @@ import SearchSection from "./components/searchSection";
 export default async function Home() {
 	const session = await getServerSession(options);
 
-	const tempObject = {
-		price: 750000,
-		address: "6 Bronsart Road, SW6 6AA",
-		date: "22/06/23",
-		type: "Terraced",
-		tenure: "Freehold",
-		distance: "150m",
-	};
-
 	return (
 		<div className="min-h-screen flex flex-col bg-gray-50">
 			<div className="container mx-auto max-w-[356px] my-6 border-2 rounded-md flex-grow">
 				<UserCard user={session?.user} />
 				<Hero />
 				<SearchSection />
-				{/* <SearchButton /> */}
-				<HouseCard record={tempObject} />
-				<HouseCard record={tempObject} />
-				<HouseCard record={tempObject} />
 			</div>
 		</div>
 	);
