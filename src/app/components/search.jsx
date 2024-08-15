a1284356b6942544f65eee23e6ea182c4d000cca
peterson-e <55142305+peterston-e@users.noreply.github.com>
2024-08-15 12:20:03 +0100

    accesability button

--- src/app/components/search.jsx
+++ src/app/components/search.jsx
@@ -1,6 +1,8 @@
 export default function Search({ onSearch, isLoading }) {
 	return (
 		<button
+			id="sold-house-search"
+			aria-label="sold-house-search"
 			onClick={onSearch}
 			disabled={isLoading}
 			className="w-full bg-blue-500 hover:bg-blue-700 active:bg-blue-800 active:transform active:scale-95 text-white font-bold py-2 px-4 rounded my-11 transition duration-150 ease-in-out"
